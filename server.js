5eab224fd3e17091dcfc3570c53d75322c1754dc
Pandaiolo <aurelien@arimaska.net>
2016-01-23 00:26:56 +0100

    Improve ES2015 parsing

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 /*eslint-disable */
 
-// Enable loading the server with babel
+// Enable loading ES2016 files with babel
 require('babel-register');
 
-var main = require('./src/main').default;
-var CONFIG = require('./config').default;
+// Resolve configuration file
+var path = require('path');
+var arg = process.argv[2] && path.resolve(process.argv[2]);
+var configFile = arg && arg.replace(/.js$/, '') || './config';
+
+// Load config
+var CONFIG = require(configFile).default;
 
 // Launch server with current config
+var main = require('./src/main').default;
 main(CONFIG);
 
