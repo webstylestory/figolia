45f96175e5f58b69e3aa82827fa85b17e854bbcd
Pandaiolo <aurelien@arimaska.net>
2016-01-28 16:02:04 +0100

    Make config.js directly usable when environment vars are set
    Also commented example schema definition to avoid unwanted indexing by
    first time users.

--- config.js
+++ config.js
@@ -13,51 +13,59 @@ import { defaultsDeep as _defaultsDeep } from 'lodash';
 let CONFIG = {
     // Firebase credentials
     firebase: {
-        instance: 'TO_BE_CHANGED',
-        secret: 'TO_BE_CHANGED',
+        instance: process.env.FIREBASE_INSTANCE || 'TO_BE_CHANGED',
+        secret: process.env.FIREBASE_SECRET || 'TO_BE_CHANGED',
         // Where to store server metadata
-        path: 'algolia',
+        path: process.env.FIREBASE_PATH || 'algolia',
         // Firebase token will be generated with this uid (to write above path)
-        uid: 'algolia'
+        uid: process.env.FIREBASE_UID || 'algolia'
     },
     // Algolia credentials
     algolia: {
-        applicationId: 'TO_BE_CHANGED',
+        applicationId: process.env.ALGOLIA_APP_ID || 'TO_BE_CHANGED',
         // *Admin* API Key
-        apiKey: 'TO_BE_CHANGED'
+        apiKey: process.env.ALGOLIA_API_KEY || 'TO_BE_CHANGED'
     },
     // Optional, this field will be checked against last
     // run date to see if reindexing is necessary.
+    // Field type must be UNIX timestamp (example Javascript Date.now()).
     // WARNING: Without this field being correctly configured,
     // everything is re-indexed at rerun.
-    timestampField: 'modifiedAt',
+    //
+//  timestampField: 'modifiedAt',
+    //
     // Firebase datasets to index in Algolia
     schema: {
-        todoLists: {
-            // Firebase path
-            path: 'app/todo',
-            // Algolia index (must exist already)
-            index: 'dev_todo_lists',
-            // Optional, name of ID field (otherwise,
-            // the Firebase object key will be used)
-            key: 'id',
-            // Optional, list of fields to index
-            // (otherwise, every field will be indexed)
-            include: [
-                'name',
-                'created',
-                'modified',
-                'itemsNumber'
-            ]
-        },
-        todoItems: {
-            // Second example dataset to index
-            path: 'app/todoItems',
-            index: 'dev_todo_items',
-            key: 'id',
-            lastModTime: 'modifiedAt',
-            backup: './backup'
-        }
+//         todoLists: {
+//             // Firebase path
+//             path: 'app/todo',
+//             // Algolia index (must exist already)
+//             index: 'dev_todo_lists',
+//             // Optional, name of ID field (otherwise,
+//             // the Firebase object key will be used)
+//             key: 'id',
+//             // Optional, list of fields to index
+//             // (otherwise, every field will be indexed)
+//             includeFields: [
+//                 'name',
+//                 'modifiedAt'
+//             ],
+//             // Optional, list of fields to exclude from index
+//             // Note: if both are specified, `excludeFields`
+//             // is applied *after* `includeFields`
+//             excludeFields: [
+//                 'passwdHash',
+//                 'private'
+//             ]
+//         },
+//         todoItems: {
+//             // Second example dataset to index
+//             path: 'app/todoItems',
+//             index: 'dev_todo_items',
+//             key: 'id',
+//             lastModTime: 'modifiedAt',
+//             backup: './backup'
+//         }
     }
 };
 
