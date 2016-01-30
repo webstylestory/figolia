4118348ede155df560213b48349e5106a4e5e18c
Pandaiolo <aurelien@arimaska.net>
2016-01-30 12:48:40 +0100

    Add some debug/info output

--- src/remove-from-index.js
+++ src/remove-from-index.js
@@ -1,3 +1,7 @@
+import Debug from './debug';
+
+const info = Debug('info:remove-from-index');
+const debug = Debug('remove-from-index');
 
 //
 // Delete an object from the index based on firebase snapshot
@@ -7,11 +11,14 @@ const deleteFromIndex = ({ fbRef, CONFIG, dataset, fb, algolia }) => {
 
   const objectID = dataset.key ? fbRef.val()[dataset.key] : fbRef.key();
 
+  info(`Removing ${dataset.path} from index with objectID ${objectID}`);
+
   const index = algolia.initIndex(dataset.index);
 
   // Remove the object from Algolia
   return index.deleteObject(objectID)
-    .then(task => index.waitTask(task.taskID));
+    .then(task => index.waitTask(task.taskID))
+    .then(() => debug(`Done removing ${dataset.path} objectID ${objectID}`))
 
 };
 
